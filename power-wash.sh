1fc76c1b141748263ff14246f228f1d9fb396ce3
Kevin Faaborg <kevin@ara.one>
2019-12-03 14:51:04 -0700

    updated from ara

--- power-wash.sh
+++ power-wash.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 echo 'power-wash.sh ---- start'
 echo ''
+echo 'Deleting package-lock.json, node_modules, and dist'
+echo 'This may take a minute...'
+echo ''
 
-rm -rf ./dist
-rm -rf ./node_modules
 rm -rf ./package-lock.json
+rm -rf ./node_modules
+rm -rf ./dist
 
-ls ./dist
-ls ./node_modules
 ls ./package-lock.json
+ls ./node_modules
+ls ./dist
 
+echo ''
+echo 'Now you can do a fresh $ npm install'
 echo ''
 echo 'power-wash.sh ---- done'
 echo ''
